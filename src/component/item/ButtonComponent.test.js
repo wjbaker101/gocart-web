8999382145d1167953e619761b13f8c139aec71b
wjbaker101 <willbaker10198@gmail.com>
2019-09-29 21:43:08 +0100

    Add test for button component

--- src/component/item/ButtonComponent.test.js
+++ src/component/item/ButtonComponent.test.js
@@ -1,15 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
 import ButtonComponent from './ButtonComponent.vue';
 
-describe('Component', () => {
+describe('ButtonComponent', () => {
     test('is a Vue instance', () => {
+        const button = shallowMount(ButtonComponent);
+
+        expect(button.isVueInstance()).toBeTruthy();
+    });
+
+    test('can become secondary button', () => {
         const button = shallowMount(ButtonComponent, {
             propsData: {
                 isSecondary: true,
             },
         });
 
-        expect(button.isVueInstance()).toBeTruthy();
+        // Check whether the 'is-secondary' class is applied to the button when
+        // 'isSecondary' is set to true
         expect(button.find('.is-secondary').exists()).toBeTruthy();
     });
 });
