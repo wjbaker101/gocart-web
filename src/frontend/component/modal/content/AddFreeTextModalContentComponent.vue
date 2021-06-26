87a983ef996708abd76ab3dacb4fbb4f126ec74e
wjbaker101 <willbaker10198@gmail.com>
2021-06-26 21:31:56 +0100

    Prevent the user from entering freetext less than 2 characters long

--- src/frontend/component/modal/content/AddFreeTextModalContentComponent.vue
+++ src/frontend/component/modal/content/AddFreeTextModalContentComponent.vue
@@ -3,7 +3,14 @@
     <p>Include an item you would like without having to search for a specific product.</p>
     <label>
         <span>Item Name</span>
-        <input ref="freetextInput" type="text" v-model="freetext" placeholder="Dairy Free Pesto" @keyup.enter="onAdd">
+        <input
+            ref="freetextInput"
+            type="text"
+            v-model="freetext"
+            placeholder="Dairy Free Pesto"
+            @keyup.enter="onAdd"
+        >
+        <UserMessageComponent :message="userMessage" />
     </label>
     <p></p>
     <ButtonComponent @click="onAdd">Add</ButtonComponent>
@@ -15,10 +22,13 @@ import { useStore } from 'vuex';
 import { v4 as uuid } from 'uuid';
 
 import ButtonComponent from '@/component/item/ButtonComponent.vue';
+import UserMessageComponent from '@/component/item/UserMessageComponent.vue';
 
 import { AppState } from '@/store/type/AppState.model';
 import { StateKeys } from '@/store/type/StateKeys';
 import { Product } from '@/model/Product.model';
+
+import { UseUserMessage } from '@/use/UserMessage.use';
 import { Event, eventService } from '@/service/Event.service';
 
 export default defineComponent({
@@ -26,13 +36,16 @@ export default defineComponent({
 
     components: {
         ButtonComponent,
+        UserMessageComponent,
     },
 
     setup() {
         const store = useStore<AppState>();
+        const useUserMessage = UseUserMessage();
 
         const freetextInput = ref<HTMLInputElement | null>(null);
         const freetext = ref<string>('');
+        const userMessage = ref<string>('');
 
         onMounted(() => {
             freetextInput.value?.focus();
@@ -41,8 +54,14 @@ export default defineComponent({
         return {
             freetextInput,
             freetext,
+            userMessage,
 
             onAdd() {
+                if (freetext.value.length < 2) {
+                    useUserMessage.set(userMessage, 'Freetext should be at least 2 characters long, please try again.');
+                    return;
+                }
+
                 const newProduct: Product = {
                     id: uuid(),
                     name: freetext.value,
