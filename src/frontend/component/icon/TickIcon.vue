33e334cd492ad54d4ad712f9f3c47bd5e7fb928e
wjbaker101 <willbaker10198@gmail.com>
2021-07-02 22:33:57 +0100

    Use an svg for checkbox ticks so they can be animated

---
+++ src/frontend/component/icon/TickIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg :width="size" :height="size" aria-hidden="true" class="icon icon-tick" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="m50.524 238.6 148.72 148.72 262.23-262.23" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="80" fill="none" />
+    </svg>
+</template>
+
+<script lang="ts">
+import { useIcon, useIconProps } from '@/use/UseIcon.use';
+
+export default {
+    name: 'TickIcon',
+
+    props: useIconProps(),
+
+    setup(props: any) {
+        return useIcon(props);
+    },
+}
+</script>
+
+<style lang="scss"></style>
--- src/frontend/component/item/CheckBoxComponent.vue
+++ src/frontend/component/item/CheckBoxComponent.vue
@@ -8,16 +8,23 @@
             @change="update"
         >
         <label :for="id" class="checkbox-label">
+            <TickIcon v-if="modelValue" />
             <span>{{ label }}</span>
         </label>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, SetupContext } from 'vue';
+import { defineComponent, ref } from 'vue';
+
+import TickIcon from '@/component/icon/TickIcon.vue';
 
 export default defineComponent({
     name: 'CheckBoxComponent',
+
+    components: {
+        TickIcon,
+    },
 
     props: {
         id: String,
@@ -25,18 +32,17 @@ export default defineComponent({
         modelValue: Boolean,
     },
 
-    setup(props: any, context: SetupContext) {
+    setup(_, { emit }) {
         const checkboxInput = ref<HTMLInputElement | null>(null);
 
         return {
             checkboxInput,
 
             update(event: Event): void {
-                if (!event || !event.target || checkboxInput.value === null) {
+                if (!event || !event.target || checkboxInput.value === null)
                     return;
-                }
 
-                context.emit('update:modelValue', checkboxInput.value.checked);
+                emit('update:modelValue', checkboxInput.value.checked);
             },
         }
     },
@@ -55,15 +61,12 @@ export default defineComponent({
                 &::before {
                     background: linear-gradient(-15deg, theme(primary-dark), theme(primary));
                 }
-
-                &::after {
-                    opacity: 1;
-                }
             }
         }
 
         .checkbox-label {
             position: relative;
+            margin-right: 0.5rem;
             cursor: pointer;
 
             * > * {
@@ -71,8 +74,27 @@ export default defineComponent({
             }
 
             span {
-                padding-left: 0.5rem;
                 font-weight: normal;
+            }
+
+            .icon-tick {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: theme(white);
+
+                path {
+                    stroke-dasharray: 600;
+                    stroke-dashoffset: 600;
+                    animation: tick-open animation(duration-mid) linear forwards;
+                }
+
+                @keyframes tick-open {
+                    to {
+                        stroke-dashoffset: 0;
+                    }
+                }
             }
 
             &::before {
@@ -82,21 +104,6 @@ export default defineComponent({
                 border: 2px solid theme(primary);
                 border-radius: layout(border-radius);
                 background-color: theme(white);
-            }
-
-            &::after {
-                content: '';
-                display: block;
-                position: absolute;
-                top: 5px;
-                left: 9px;
-                pointer-events: none;
-                opacity: 0;
-                width: 6px;
-                height: 11px;
-                border-bottom: 2px solid theme(white);
-                border-right: 2px solid theme(white);
-                transform: rotate(45deg);
             }
         }
     }
