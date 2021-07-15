e15499a1264d9a976c7303e316bdf426a359e67d
wjbaker101 <willbaker10198@gmail.com>
2021-07-15 22:12:24 +0100

    Delete productData api since it wasn't used and has been deleted from the backend

--- src/frontend/api/tesco/Tesco.api.ts
+++ src/frontend/api/tesco/Tesco.api.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { ApiResponse } from '@/api/type/ApiResponse';
 import { AxiosErrorMapper } from '@/api/mapper/AxiosErrorMapper';
 import { SearchProduct } from '@/api/tesco/type/SearchProduct';
-import { TescoProduct } from '@/api/tesco/type/TescoProduct';
 import { TescoShop } from '@/api/tesco/type/TescoShop';
 
 const tescoApi = axios.create({
@@ -17,20 +16,6 @@ export const TescoApi = {
             const url = `/product/search/${searchTerm}/${page}`;
 
             const response = await tescoApi.get<ApiResponse<SearchProduct[]>>(url);
-
-            return response.data.result;
-        }
-        catch (exception) {
-            return AxiosErrorMapper.map(exception);
-        }
-    },
-
-    async productData(productId: string): Promise<TescoProduct | Error> {
-        try {
-            const url = `/product/${productId}`;
-
-            const response =
-                await tescoApi.get<ApiResponse<TescoProduct>>(url);
 
             return response.data.result;
         }
